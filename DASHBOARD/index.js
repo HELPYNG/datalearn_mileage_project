7329b242a44e06f0b8d4184192e6d347489f42e8
DaniloCossioloDias <116849332+DaniloCossioloDias@users.noreply.github.com>
2023-10-11 22:08:20 -0300

    Add files via upload

--- DASHBOARD/index.js
+++ DASHBOARD/index.js
@@ -1,63 +1,22 @@
-function fundoBranco() {
-    const body = document.body;
-    const menu = document.querySelector('.menu');
-    const grafico1 = document.querySelector('.grafico1');
-    const grafico2 = document.querySelector('.grafico2');
-    const grafico3 = document.querySelector('.grafico3');
-    const grafico4 = document.querySelector('.grafico4');
-    const info = document.querySelector('.info');
-    const thElements = document.querySelectorAll('th');
-    const tdElements = document.querySelectorAll('td');
+function darkMode() {
+    var body = document.body;
+    body.classList.toggle("dark-mode");
 
-    body.style.backgroundColor = 'white';
-    menu.style.backgroundColor = '#c2c7ca';
-    grafico1.style.backgroundColor = '#c2c7ca';
-    grafico2.style.backgroundColor = '#c2c7ca';
-    grafico3.style.backgroundColor = '#c2c7ca';
-    grafico4.style.backgroundColor = '#c2c7ca';
-    info.style.backgroundColor = '#c2c7ca';
-    thElements.forEach(function(th) {
-        th.style.backgroundColor = '#777b80';
-    });
-
-    tdElements.forEach(function(td) {
-        td.style.backgroundColor = '#cccfd1';
-    });
-    
-    
+    var dark_mode = body.classList.contains("dark-mode");
+    if(dark_mode) {
+        body.style.backgroundColor = '#434444';    
+        localStorage.setItem('darkMode', 'ativado');
+    } else {
+        body.style.backgroundColor = '';
+        localStorage.setItem('darkMode', 'desativado');
+    }   
 }
 
-    const mudarFundoBranco = document.getElementById('fundoBranco');
-    mudarFundoBranco.addEventListener(click, fundoBranco());
-
-
-function fundoEscuro() {
-    const body = document.body;
-    const menu = document.querySelector('.menu');
-    const grafico1 = document.querySelector('.grafico1');
-    const grafico2 = document.querySelector('.grafico2');
-    const grafico3 = document.querySelector('.grafico3');
-    const grafico4 = document.querySelector('.grafico4');
-    const info = document.querySelector('.info');
-    const thElements = document.querySelectorAll('th');
-    const tdElements = document.querySelectorAll('td');
-
-    body.style.backgroundColor = '';
-    menu.style.backgroundColor = '';
-    grafico1.style.backgroundColor = '';
-    grafico2.style.backgroundColor = '';
-    grafico3.style.backgroundColor = '';
-    grafico4.style.backgroundColor = '';
-    info.style.backgroundColor = '';
-    thElements.forEach(function(th) {
-        th.style.backgroundColor = '';
-    });
-
-    tdElements.forEach(function(td) {
-        td.style.backgroundColor = '';
-    });
-}
-
-    const mudarFundoEscuro = document.getElementById('fundoEscuro');
-    mudarFundoEscuro.addEventListener(click, fundoEscuro())
-
+document.addEventListener('DOMContentLoaded', function () {
+    var body = document.body;
+    var darkModeStatus = localStorage.getItem('darkMode');
+    if (darkModeStatus === 'ativado') {
+        body.classList.add('dark-mode');
+        body.style.backgroundColor = '#434444';
+    }
+});
