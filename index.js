87653f64db7d48df762342ad3b39ebd11a8beb57
DaniloCossioloDias <116849332+DaniloCossioloDias@users.noreply.github.com>
2023-11-01 22:03:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-function darkMode() {
-    var body = document.body;
-    body.classList.toggle("dark-mode");
+(async ()=>{
+    const db=require('./db')
+    //console.log('Inserir nova variavel')
+    //await db.insereVariavel({nome:'nome',idade:'20'})
+    //const nome='nome1'
+    //const idade='19'
 
-    var dark_mode = body.classList.contains("dark-mode");
-    if(dark_mode) {
-        body.style.backgroundColor = '#434444';    
-        localStorage.setItem('darkMode', 'ativado');
-    } else {
-        body.style.backgroundColor = '';
-        localStorage.setItem('darkMode', 'desativado');
-    }   
-}
+    //console.log('Obter todas as variaveis')
+    //const variaveis=await db.nomeDaVariavel()
+    //console.log(variaveis)
 
-document.addEventListener('DOMContentLoaded', function () {
-    var body = document.body;
-    var darkModeStatus = localStorage.getItem('darkMode');
-    if (darkModeStatus === 'ativado') {
-        body.classList.add('dark-mode');
-        body.style.backgroundColor = '#434444';
-    }
-});
+    
+    //console.log('Atualizar variavel')
+    //await db.insereVariavel({nome:'nome',idade:'20'})
+    //const nome='nome1'
+    //const idade='19'
+    //const id=2
+    //await db.atualizaVariavel({'variavel' + id + 'atualizado'})
 
-
-
+    //const id=3
+    //console.log('deletar variavel')
+    //await db.atualizaVariavel({'variavel' + id + 'deletado'})
+})()
+//comando node index no node.js
